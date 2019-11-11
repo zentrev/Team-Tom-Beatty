3a6a48c94766caf4c589564122fda808dcc92f3b
Kali <c.t.raines.iii@gmail.com>
2019-11-11 11:22:43 -0700

    init

---
+++ Index.php
@@ -0,0 +1,28 @@
+<?php
+class simpleCMS {
+    var $host;
+    var $username;
+    var $password;
+    var $table;
+  
+    public function display_public() {
+      
+    }
+  
+    public function display_admin() {
+      
+    }
+  
+    public function write() {
+      
+    }
+  
+    public function connect() {
+      
+    }
+  
+    private function buildDB() {
+      
+    }
+  }
+?>
