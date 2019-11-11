e98c2acbb050e914128dc1ba24e84c2b92e6af84
Kali <c.t.raines.iii@gmail.com>
2019-11-11 11:32:08 -0700

    comments

--- CMS.php
+++ CMS.php
@@ -1,26 +1,33 @@
 <?php
     class simpleCMS {
+
+        // Server Info
         var $host;
         var $username;
         var $password;
         var $table;
     
+        // Public Page
         public function display_public() {
         
         }
     
+        // Admin page
         public function display_admin() {
         
         }
-    
+
+        // Sava info to Database
         public function write() {
         
         }
     
+        // Connect To Database
         public function connect() {
         
         }
     
+        // Build Database if none exist
         private function buildDB() {
         
         }
