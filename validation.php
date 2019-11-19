1f9a397f131ac78dadbeb887213980b1f66eab4b
Yoannyz <yzamora@student.neumont.edu>
2019-11-19 13:42:48 -0700

    Update validation.php

--- validation.php
+++ validation.php
@@ -2,4 +2,18 @@
 $_POST["username"];
 $_POST["password"];
 
+include "dbConfig.php";
+
+$query = "SELECT * FROM users WHERE 'name' like $_POST["username"] WHERE 'password' like $_POST["password"]";
+$sqlResult = $mysqli->query( $query );
+$rows = array();
+while($r = mysqli_fetch_assoc($sqlResult))
+{
+    $rows[] = $r;
+}
+if($rows != null) {
+    session_start();
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
+}
 ?>
